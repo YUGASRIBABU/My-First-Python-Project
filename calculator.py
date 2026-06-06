ee9a5445c8d8dc613ba0f28f3d9aada6cb24738d
YUGASRI BABU <yugasrib@gmail.com>
2026-06-06 22:07:34 +0530

    Fix typo in variable name for total power calculation

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ data_node_A = int(input("Enter Node A Power: "))
 data_node_B = int(input("Enter Node B Power: "))
 
 # Calculator the sum dynamically
-total_power = data_node_A + data_noad_B
+total_power = data_node_A + data_node_B
 
 print("\n=====================================")
 print(f"Node A output: {data_node_A}")
