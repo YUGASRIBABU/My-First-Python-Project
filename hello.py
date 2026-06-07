3f412e8f44af961e9642e27bf491f06cdf293c08
YUGASRIBABU <yugasrib@gmail.com>
2026-06-07 23:44:09 +0530

    Streak save

--- hello.py
+++ hello.py
@@ -1,11 +1,1 @@
-# My very first python program!
-# System Diagnostics Profile
-user_name = "Yugasri Babu"
-college_node = "SRM Ramapuram"
-current_level = 1
-
-print("============== SYSTEM INITIALIZED ==============")
-print(f"Cadet Name: {user_name}")
-print(f"Sector Node: {college_node}")
-print(f"Python Training Level: {current_level}")
-print("================================================")
+# Day 1 of my 100 day coding challenge!
