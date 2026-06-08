162db42263939d754f3edb5f9d9bf0564b6f662e
YUGASRIBABU <yugasrib@gmail.com>
2026-06-08 20:31:08 +0530

    Built the Dentist Escape game using escape sequences

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
-# Day 2: Playing the String
-first_name = "Yuga"
-greeting = "Welcome to Day 2 of the coding challengr!"
+# Day 2: The Dentist Escape Room
+print("--- THE DENTIST ESCAPE ---")
 
-# Glues the string together using the sign
-full_message = greeting + "Let's go, " + "!"
+# \n creates a new line, and \t indents the text like a bullet point!
+print("You wake up trapped in the dentist chair...\n")
+print("Your choices:\n\t1. Fight the dentist\n\t2. Jump out the window")
 
-print(full_message)
+# This lets you type your choice in the terminal!
+choice = input("\nType 1 or 2 and press Enter: ")
+
+print("\n--- RESULTS ---")
+print("You chose option: " + choice)
+print("The dentist says: \"You can\'t escape cleaning day!\"")
