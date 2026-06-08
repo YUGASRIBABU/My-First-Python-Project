b203d74e7b32ce1b7ddc2cf85065981e524b8a44
YUGASRIBABU <yugasrib@gmail.com>
2026-06-08 19:22:18 +0530

    Learned about string and survived the dentist

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-# Day 1 of my 100 day coding challenge!
+# Day 2: Playing the String
+first_name = "Yuga"
+greeting = "Welcome to Day 2 of the coding challengr!"
+
+# Glues the string together using the sign
+full_message = greeting + "Let's go, " + "!"
+
+print(full_message)
