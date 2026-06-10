3c06fe0753c1b497c466716ca1302e69373511ca
YUGASRIBABU <yugasrib@gmail.com>
2026-06-10 23:03:21 +0530

    feat: implement battery drain tracking calculation
    Created int and float variables to calculate remaining laptop charge dynamically.

--- day_04_strings.py
+++ day_04_strings.py
@@ -2,3 +2,14 @@ my_name = "Yugasri Babu"
 name_length = len(my_name)
 
 print(f"My name is {my_name} and it has {name_length} letters!")
+
+# 1. force the whole name to UPPERCASE
+print(my_name.upper())
+
+# 2. Swap out "Babu" for "The Developer"
+hero_name = my_name.replace("Babu", "The Developer")
+print(hero_name)
+
+# 3. Clean up accidental space automatically
+dirty_input = "  Microsof AI Fest 2026  "
+print(dirty_input.strip())
