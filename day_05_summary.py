8d54bf488f733671d9d60e46d4825a9cf6a6b411
YUGASRIBABU <yugasrib@gmail.com>
2026-06-11 22:55:45 +0530

    Day 5: Mastered logical operators and fixed environmental setting

---
+++ day_05_summary.py
@@ -0,0 +1,16 @@
+# 1.Day 5: My Real Progress
+hours_learned = 4
+coad_runner_fixed = True
+feeling_tired = True
+
+print("---Yuga's Day 5 Python Journal---")
+
+# testing chained comparison operators (Maths Style)
+if 1 <= hours_learned <= 3:
+    print("Study session length was realising for today.")
+
+# 2. Testing logical operators with today's real situation
+if coad_runner_fixed and not feeling_tired:
+    print("I have plenty of energy to keep coding")
+else:
+    print("VS Coad is fixed, but my brain is tired!")
