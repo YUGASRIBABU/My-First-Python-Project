f03104f970820ad2c8af82bb65ce21f72fba9622
YUGASRIBABU <yugasrib@gmail.com>
2026-06-13 14:21:51 +0530

    Day 7:Mastered dynamic variable self-updates and arithmetic operators

---
+++ day_07_score_multiplier.py
@@ -0,0 +1,14 @@
+# start with 10
+score = 10
+
+# 1. Increase by 5
+score = score + 5
+
+# 2. Triple it (Using the shorthand format this time!)
+score *= 3
+
+# 3.Decrease by 2
+score -= 2
+
+# Check the final result
+print(score)
